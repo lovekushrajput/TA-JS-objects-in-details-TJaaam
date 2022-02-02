7b9a981f2bf32e13616d620a2a1606f323ababec
lovekush rajput <lovekushrazput143@gmail.com>
2022-02-02 07:16:23 +0530

    done

--- block-BJaaik/code/index.js
+++ block-BJaaik/code/index.js
@@ -19,47 +19,51 @@ let person = new Person("Lovekush",18,"Male")
 
 
 // Player
-class Player {
- constructor(sportsname){
+class Player extends Person{
+ constructor(name,age,gender,sportsname){
+    super(name,age,gender)
 this.sportsname = sportsname
  };
  play(){
-return `is playing ${this.sportsname}`
+return `${this.name} is playing ${this.sportsname}`
  }
 }
 
-let player = new Player(person)
+let player = new Player("Dhoni",48,"Male","Cricket")
 
 
-class Cricketer {
-    constructor(teamName){
+class Cricketer extends Player {
+    constructor( name,age,gender,sportsname,teamName){
+        super(name,age,gender,sportsname)
    this.teamName = teamName
     };
     playCricket(){
-   return `${this.teamName} is good team`
+   return `${this.name} is ${this.sportsname} of ${this.teamName}`
     }
    }
 
-   let cricketer = new Cricketer(player)
+   let cricketer = new Cricketer("Virat kohli",34,"Male","Cricketer","India")
 
 // Teacher   
-class Teacher {
-    constructor(instituteName){
+class Teacher extends Person {
+    constructor(name,age,gender,instituteName){
+        super(name,age,gender)
    this.instituteName = instituteName
     };
     teach(){
-   return `is teaching in ${this.instituteName}`
+   return `Mrs.${this.name} is  teaching in ${this.instituteName}`
     }
    }
-let teacher = new Teacher(person)
+let teacher = new Teacher("Rose",24,"Female","BBD University")
 
  // Artist
-   class  Artist {
-    constructor(kind){
+   class  Artist extends Person{
+    constructor(name,age,gender,kind){
+    super(name,age,gender)
    this.kind = kind
     };
     createArt(){
-   return `is a ${this.kind}`
+   return `${this.name} is an ${this.kind}`
     }
    }
-let artist = new Artist(person)
+let artist = new Artist("Manoj Bajpayee",43,"Male","Actor")
